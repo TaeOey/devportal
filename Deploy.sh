0766841221f41abd27a8e91ab42a97570ac2f0cb
Joe DeRosa <jderosa@emoneyadvisor.com>
2019-09-09 15:20:37 -0400

    symlink

--- Deploy.sh
+++ Deploy.sh
@@ -2,8 +2,9 @@
 
 #Declare variables
 CWD=`pwd`
-APIGEE_DRUPAL_SOURCE_ROOT=/var/www/devportal/"#{Octopus.Release.Number}"
+APIGEE_DRUPAL_SOURCE_ROOT=/var/www/devportal/
 APIGEE_DRUPAL_WEB_DOCROOT=/var/www/devportal/web
+APIGEE_DRUPAL_SOURCE_ROOT_RELEASE=${APIGEE_DRUPAL_SOURCE_ROOT}/"#{Octopus.Release.Number}"
 EMONEY_DEVPORTAL_PROJECT_DIRECTORY=/opt/apigee/data/apigee-drupal-devportal/sites/all
 PACKAGE_ID=`basename $(pwd)`
 CURRENT_DATETIME=`date +%Y%m%d-%H%M%S`
@@ -38,10 +39,7 @@ ${CWD}/drush version
 #Backup Drupal database
 echo "Create database backup in ${BACKUP_DIRECTORY}/${DB_BACKUP}"
 echo "${DB_IP}:${DB_PORT}:${DB_NAME}:${DB_USER}"
-#sudo cd ${APIGEE_DRUPAL_WEB_DOCROOT}
-sudo chmod -R 777 /var/www/devportal/vendor/bin
-sudo /var/www/devportal/vendor/bin/drush sql-dump > ${BACKUP_DIRECTORY}/${DB_BACKUP}
-
+sudo ${CWD}/drush --root=${APIGEE_DRUPAL_WEB_DOCROOT} sql-dump > ${BACKUP_DIRECTORY}/${DB_BACKUP}
 
 #Backup Drupal data - not necessary??
 # echo "Create drupal directories backup in ${BACKUP_DIRECTORY}/${DRUPAL_BACKUP}"
@@ -54,44 +52,27 @@ sudo /var/www/devportal/vendor/bin/drush sql-dump > ${BACKUP_DIRECTORY}/${DB_BAC
 
 echo "Creating and Fixing Permission On ${APIGEE_DRUPAL_SOURCE_ROOT}"
 
-sudo mkdir ${APIGEE_DRUPAL_SOURCE_ROOT}
+sudo mkdir ${APIGEE_DRUPAL_SOURCE_ROOT_RELEASE}
 
-sudo cd ${APIGEE_DRUPAL_SOURCE_ROOT}
-sudo pwd
+sudo rsync -r * ${APIGEE_DRUPAL_SOURCE_ROOT_RELEASE}
 
-sudo rsync -r * ${APIGEE_DRUPAL_SOURCE_ROOT}
-
-sudo find ${APIGEE_DRUPAL_SOURCE_ROOT} -type d -exec chmod 755 {} \;
-sudo find ${APIGEE_DRUPAL_SOURCE_ROOT} -type f -exec chmod 644 {} \;
-sudo find ${APIGEE_DRUPAL_SOURCE_ROOT}/web/sites/default/ -type d -exec chmod 775 {} \;
-#sudo find ${APIGEE_DRUPAL_SOURCE_ROOT}/vendor/bin -type d -exec chmod -R 777 {} \;
+sudo find ${APIGEE_DRUPAL_SOURCE_ROOT_RELEASE} -type d -exec chmod 755 {} \;
+sudo find ${APIGEE_DRUPAL_SOURCE_ROOT_RELEASE} -type f -exec chmod 644 {} \;
+sudo find ${APIGEE_DRUPAL_SOURCE_ROOT_RELEASE}/web/sites/default/ -type d -exec chmod 775 {} \;
 #sudo find ${APIGEE_DRUPAL_SOURCE_ROOT}/web/sites/default/files -type d -exec chmod 775 {} \;
 
-
-#Update codebase to actual version (this I need help with to figure out
-# echo "Updating codebase"
-# for item in ${DRUPAL_DIR_LIST}; do
-#     echo "Deploying ${item}"
-#     sudo rsync -av --delete ${item}/ ${APIGEE_DRUPAL_WEB_DOCROOT}/${item}
-    
-# done
+#Fix symlink
+sudo ln -sf ${APIGEE_DRUPAL_SOURCE_ROOT_RELEASE} ${APIGEE_DRUPAL_SOURCE_ROOT}
 
 #Initialize updates:
 echo "Initializing updates"
-#sudo chmod -R 777 ${APIGEE_DRUPAL_SOURCE_ROOT}/vendor/bin
-sudo /var/www/devportal/vendor/bin/drush version
-#sudo && ${APIGEE_DRUPAL_SOURCE_ROOT}/vendor/bin/drush -v
-#sudo && ${APIGEE_DRUPAL_SOURCE_ROOT}/vendor/bin/drush sql-dump > ${BACKUP_DIRECTORY}/${DB_BACKUP}
-#sudo && ${APIGEE_DRUPAL_SOURCE_ROOT}/vendor/bin/drush updb -y
-
+sudo ${CWD}/drush --root=${APIGEE_DRUPAL_WEB_DOCROOT} drush updb -y
 
 #Actualize configuration layer:
 echo "Actualize configuration layer"
-#sudo ${APIGEE_DRUPAL_SOURCE_ROOT}/vendor/bin/drush cim -y
+sudo ${CWD}/drush --root=${APIGEE_DRUPAL_WEB_DOCROOT} drush cim -y
 
 #Clear caches:
-#sudo ${APIGEE_DRUPAL_SOURCE_ROOT}/vendor/bin/drush cr
-
-#Move Symlink
+sudo ${CWD}/drush --root=${APIGEE_DRUPAL_WEB_DOCROOT} drush cr
 
 #Delete old versions
