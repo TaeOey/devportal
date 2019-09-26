8a0f9cb5bc2a091be5928b293de6c019509a964c
Pavel Druzhinin <pdruzhinin@emoneyadvisor.com>
2019-09-26 15:13:19 +0200

    Creating rollback script

--- dbbackup.sh
+++ dbbackup.sh
@@ -25,6 +25,15 @@ echo "Create database backup in ${BACKUP_DIRECTORY}/${DB_BACKUP}"
 echo "${DB_IP}:${DB_PORT}:${DB_NAME}:${DB_USER}"
 sudo mysqldump --user ${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} | gzip > ${BACKUP_DIRECTORY}/${DB_BACKUP}
 
+#Create a database rollback script
+sudo cat << 'EOF' >> ${BACKUP_DIRECTORY}/Rollback-${DB_BACKUP}.sh
+echo "Restoring database backup ${DB_BACKUP}"
+mysql --user ${DB_USER} --password=${DB_PASSWORD} -D ${DB_NAME} < ${BACKUP_DIRECTORY}/${DB_BACKUP}
+EOF
+
+chmod +x ${BACKUP_DIRECTORY}/Rollback-${DB_BACKUP}.sh
+echo "Rollback script created at ${BACKUP_DIRECTORY}/Rollback-${DB_BACKUP}.sh"
+
 #Delete old database backups
 # echo "Cleaning up old backups"
 # DB_BACKUP_PATTERN=`sudo echo $DB_BACKUP | sed -E 's/[[:digit:]]{8}-[[:digit:]]{6}/*/g'`
