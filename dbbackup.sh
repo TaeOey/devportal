2ce240a36b07ae63e3018673fcf67a249e731e61
Joe DeRosa <jderosa@emoneyadvisor.com>
2019-09-25 21:06:49 -0400

    fix db password variable

--- dbbackup.sh
+++ dbbackup.sh
@@ -23,13 +23,16 @@ fi
 #Backup Drupal database
 echo "Create database backup in ${BACKUP_DIRECTORY}/${DB_BACKUP}"
 echo "${DB_IP}:${DB_PORT}:${DB_NAME}:${DB_USER}"
-sudo mysqldump --user ${DB_USER} --password "#{DrupalPassword}" ${DB_NAME} | gzip > ${BACKUP_DIRECTORY}/${DB_BACKUP}
+sudo mysqldump --user ${DB_USER} --password ${DB_PASSWORD} ${DB_NAME} | gzip > ${BACKUP_DIRECTORY}/${DB_BACKUP}
 
 # #Delete old database backups
+echo "Cleaning up old backups"
 DB_BACKUP_PATTERN=`sudo echo $DB_BACKUP | sed -E 's/[[:digit:]]{8}-[[:digit:]]{6}/*/g'`
 sudo ls -t ${BACKUP_DIRECTORY}/${DB_BACKUP_PATTERN} | tail -n +4 | xargs rm --
 
+echo "Checking for remote backup directory"
 if [ ! -d "${REMOTE_BACKUP_DIRECTORY}" ]; then
+    echo "Sending backup to remote backup directory"
     cp ${BACKUP_DIRECTORY}/${DB_BACKUP} ${REMOTE_BACKUP_DIRECTORY}/${DB_BACKUP}
 fi
 
