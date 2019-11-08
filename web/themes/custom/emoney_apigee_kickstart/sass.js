90f811573f7ac4d59a7e40d42cfb67dad0fcde59
Leandro Cunha <lcunha@emoneyadvisor.com>
2019-11-08 10:22:18 -0500

    Add autoprefixer node module

--- web/themes/custom/emoney_apigee_kickstart/sass.js
+++ web/themes/custom/emoney_apigee_kickstart/sass.js
@@ -1,22 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 const sass = require('node-sass');
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss');
 
-const filename = 'emoney_style_sheet';
+const filename = 'emoney_style_sheet.css';
 
 sass.render({
   file: 'sass/app.scss',
   outputStyle: 'compressed',
-  outFile: path.resolve(__dirname, 'assets', 'css') + '/' + filename + '.css',
+  outFile: path.resolve(__dirname, 'assets', 'css') + '/' + filename,
 }, function(error, result) {
   if(!error){
-    fs.writeFile(
-      path.resolve(__dirname, 'assets', 'css') + '/' + filename + '.css',
-      result.css,
-      function(err) {
-        if (err) throw err;
-        console.log(filename + ' compiled with success!', '\x1b[32m', '[ok]', '\t\t\x1b[0m');
-      });
+    postcss([autoprefixer])
+    .process(result.css, { from: 'assets/css/' + filename, to: 'assets/css/' + filename })
+    .then(result => {
+      fs.writeFile(
+        path.resolve(__dirname, 'assets', 'css') + '/' + filename,
+        result.css,
+        err => {
+          if (err) throw err;
+          console.log(filename + ' compiled with success!', '\x1b[32m', '[ok]', '\t\t\x1b[0m');
+        });
+    });
   }
   else{
     console.log(error);
